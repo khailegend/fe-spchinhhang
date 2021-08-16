66cca31d49725f57a83ec8afb36c5490c7a69671
khailegend <nhanchikhai@gmail.com>
2021-08-16 22:18:36 +0700

    update catalog , add catalog menu

--- src/layouts/Header/Catalog.tsx
+++ src/layouts/Header/Catalog.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 import ReorderIcon from '@material-ui/icons/Reorder';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import Menu from '@material-ui/core/Menu';
 
 const CatalogWrapped = styled.div`
   color: white;
+  cursor: pointer;
 `;
-const CatalogLogo = styled.div`
+const CatalogLogo = styled.a`
   display: flex;
   font-size: 30px;
+  align-items: center;
+  font-size: 13px;
 `;
 const CatalogTitle = styled.div`
   display: flex;
@@ -17,10 +21,10 @@ const CatalogTitle = styled.div`
   flex-flow: column wrap;
 `;
 const SmallTitle = styled.span`
-  font-size: 12px;
+  font-size: 11px;
 `;
 const BigTitle = styled.span`
-  font-weight: 600;
+  font-weight: 500;
   font-size: 13px;
   display: flex;
 `;
@@ -31,10 +35,54 @@ const IconDropDown = styled(ArrowDropDownIcon)`
   }
 `;
 
+const CatalogMenu = styled(Menu)`
+  && {
+    margin-top: 4%;
+    padding: 0;
+  }
+`;
+
+const MenuItem = styled.li`
+  list-style: none;
+  background-color: white;
+  border-color: white;
+  color: black;
+  border-style: solid solid none;
+  border-width: 1px 1px 0px;
+  display: list-item;
+  font-size: 14px;
+  cursor: pointer;
+  &:hover {
+    background-color: #1a94ff;
+    border-color: #1a94ff;
+    color: white;
+  }
+`;
+
+const MenuLink = styled.a`
+  align-items: center;
+  display: flex;
+`;
+
+const MenuTitle = styled.span`
+  line-height: 14.95px;
+  padding: 10px 10px;
+`;
+
 export default function Catalog() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <CatalogWrapped>
-      <CatalogLogo>
+      <CatalogLogo onMouseOver={handleClick} aria-controls="fade-menu" aria-haspopup="true">
         <ReorderIcon style={{ fontSize: 40 }}></ReorderIcon>
         <CatalogTitle>
           <SmallTitle>Danh mục </SmallTitle>
@@ -44,6 +92,24 @@ export default function Catalog() {
           </BigTitle>
         </CatalogTitle>
       </CatalogLogo>
+
+      <CatalogMenu
+        id="fade-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}>
+        <MenuItem onClick={handleClose}>
+          <MenuLink>
+            <MenuTitle>Điện thoại - Máy tính bảng</MenuTitle>
+          </MenuLink>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <MenuLink>
+            <MenuTitle>Điện tử - Điện lạnh</MenuTitle>
+          </MenuLink>
+        </MenuItem>
+      </CatalogMenu>
     </CatalogWrapped>
   );
 }
